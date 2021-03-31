2ce9066b713ea8605dbf1f9ecd5baa943fc1fa18
panoskyr <35936229+panoskyr@users.noreply.github.com>
2021-03-31 21:34:13 +0300

    added add calendar to create tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,5 @@
 BEGIN;
+\i 'C:/Users/pchrk/OneDrive/Desktop/cs/2nd year/d examino/vaseis/vaseis 2021/airbnb dataset/sql files/create_calendar.sql'
 \i 'C:/Users/pchrk/OneDrive/Desktop/cs/2nd year/d examino/vaseis/vaseis 2021/airbnb dataset/sql files/create_neighbourhoods.sql'
 \i 'C:/Users/pchrk/OneDrive/Desktop/cs/2nd year/d examino/vaseis/vaseis 2021/airbnb dataset/sql files/create_geolocation.sql'
 \i 'C:/Users/pchrk/OneDrive/Desktop/cs/2nd year/d examino/vaseis/vaseis 2021/airbnb dataset/sql files/create_listings.sql'
