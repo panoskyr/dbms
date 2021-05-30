db8787b1f63dc134e1474fd3de8bca32baae21ca
GKotsifos <54845904+DeepBlueIBM@users.noreply.github.com>
2021-05-30 16:40:50 +0300

    Update query_indexes.sql

--- ergasies/e6/query_indexes.sql
+++ ergasies/e6/query_indexes.sql
@@ -1,20 +1,16 @@
 --QUERY 1
-CREATE INDEX host_id_index_for_listing ON Listing(host_id);
+CREATE INDEX host_id_index_for_listing ON Listing2(host_id);
 DROP INDEX host_id_index_for_listing;
---to host_id(id) einai PK ston host ara den xreiazetai index
 
 
 --------------------------------------------------------------
 --QUERY 2
-CREATE INDEX guests_included_index_for_listing ON Listing(guests_included);
-DROP INDEX guests_included_index_for_listing;
-
-CREATE INDEX price_index_for_price ON Price(price);
-DROP INDEX price_index_for_price;
+CREATE INDEX guests_included_price_index_for_price ON Price(price,guests_included);
+DROP INDEX guests_included_price_index_for_price;
 
 --------------------------------------------------------------
 --QUERY 3
-CREATE INDEX beds_index_for_room ON Room(beds);
+CREATE INDEX beds_index_for_room ON Room2(beds);
 DROP INDEX beds_index_for_room;
 
 CREATE INDEX guests_included_index_for_price ON Price(guests_included);
@@ -22,21 +18,20 @@ DROP INDEX guests_included_index_for_price;
 
 --------------------------------------------------------------
 --QUERY 4
-CREATE INDEX listing_id_index_for_listing ON Listing(id);
+CREATE INDEX listing_id_index_for_listing ON Listing2(id);
 DROP INDEX listing_id_index_for_listing;
 --einai hdh PK
 
 
 --------------------------------------------------------------
 --QUERY 5
-CREATE INDEX host_id_index_for_listing ON Listing(host_id);
+CREATE INDEX host_id_index_for_listing ON Listing2(host_id);
 DROP INDEX host_id_index_for_listing;
---Isos den xreiazetai index epeidh den yparxei Where/Having??
 
 
 --------------------------------------------------------------
 --QUERY 6
-CREATE INDEX has_availability_index_for_listing ON Listing(has_availability);
+CREATE INDEX has_availability_index_for_listing ON Listing2(has_availability);
 DROP INDEX has_availability_index_for_listing;
 
 CREATE INDEX is_superhost_index_for_host ON Host(is_superhost);
@@ -45,4 +40,5 @@ DROP INDEX is_superhost_index_for_host;
 
 --------------------------------------------------------------
 --QUERY 7
---Isos den xreiazetai index epeidh den yparxei Where/Having??
+CREATE INDEX geometry_coordinates_index_for_geolocation ON Geolocation2(geometry_coordinates_0_0_7_0);
+DROP INDEX geometry_coordinates_index_for_geolocation;
